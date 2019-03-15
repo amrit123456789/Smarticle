3b8300c3a78a285f00f0d4dd5488fc289ac43d5d
amrit123456789 <amritagarwal98@gmail.com>
2019-03-15 14:04:08 +0530

    profile api completed
    Signed-off-by: amrit123456789 <amritagarwal98@gmail.com>

--- src/routes/api/profiles.js
+++ src/routes/api/profiles.js
@@ -7,9 +7,8 @@ var auth =require('../../middlewares/auth')
 
 // Preload user profile on routes with ':username'
 route.param('username', function(req, res, next, username){
-  User.findOne({
-    where:{username: username}
-  }).then(function(user){
+  console.log("in param")
+  User.findOne({username: username}).then(function(user){
     if (!user) { return res.sendStatus(404); }
 
     req.profile = user;
@@ -19,6 +18,7 @@ route.param('username', function(req, res, next, username){
 });
 
 route.get('/:username',auth.optional, function(req, res, next){
+  console.log("in get")
   if(req.payload){
     User.findById(req.payload.id).then(function(user){
       if(!user){ return res.json({profile: req.profile.toProfileJSONFor(false)}); }
